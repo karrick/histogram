3698a9ecea90a8128d5193f62b3c03de65de3c25
Karrick S. McDermott <karrick@karrick.net>
2018-03-07 21:19:24 +0000

    refactored to pull out gohistogram library

--- main.go
+++ main.go
@@ -4,19 +4,22 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
+	"github.com/karrick/gobls"
+	"github.com/karrick/gohistogram"
 	"github.com/karrick/golf"
 	"github.com/karrick/gorill"
 )
 
 var (
-	optDelimiter  = golf.StringP('d', "delimiter", "", "specify alternative field delimiter (Default: empty string implies any whitespace")
-	optField      = golf.IntP('f', "field", 0, "specify input field (Default: 0 implies entire line")
-	optFold       = golf.Bool("fold", false, "fold duplicate keys")
-	optPercentage = golf.BoolP('p', "percentage", false, "show percentage")
-	optSortAsc    = golf.Bool("ascending", false, "print histogram in ascending order")
-	optSortDesc   = golf.Bool("descending", false, "print histogram in descending order")
-	optWidth      = golf.IntP('w', "width", 80, "width of output histogram")
+	optDelimiter = golf.StringP('d', "delimiter", "", "specify alternative field delimiter (Default: empty string implies any whitespace")
+	optField     = golf.IntP('f', "field", 0, "specify input field (Default: 0 implies entire line")
+	optFold      = golf.Bool("fold", false, "fold duplicate keys")
+	optPercent   = golf.BoolP('p', "percentage", false, "show percentage")
+	optSortAsc   = golf.Bool("ascending", false, "print histogram in ascending order")
+	optSortDesc  = golf.Bool("descending", false, "print histogram in descending order")
+	optWidth     = golf.IntP('w', "width", 80, "width of output histogram")
 )
 
 func main() {
@@ -29,8 +32,8 @@ func main() {
 		ior = &gorill.FilesReader{Pathnames: golf.Args()}
 	}
 
-	hist := new(histogram)
-	err := hist.Ingest(ior, *optField, *optDelimiter)
+	hist := new(gohistogram.Strings)
+	err := ingest(ior, hist, *optField, *optDelimiter)
 	if err != nil {
 		exit(err)
 	}
@@ -45,7 +48,7 @@ func main() {
 		hist.SortAscending()
 	}
 
-	if *optPercentage {
+	if *optPercent {
 		exit(hist.PrintWithPercent(*optWidth))
 	} else {
 		exit(hist.Print(*optWidth))
@@ -58,4 +61,34 @@ func exit(err error) {
 		os.Exit(1)
 	}
 	os.Exit(0)
+}
+
+func ingest(ior io.Reader, hist *gohistogram.Strings, field int, delimiter string) error {
+	scanner := gobls.NewScanner(ior)
+	for scanner.Scan() {
+		// split line into fields
+		var fields []string
+		key := scanner.Text()
+		if field == 0 {
+			key = strings.TrimSpace(key)
+		} else {
+			if delimiter == "" {
+				fields = strings.Fields(key)
+			} else {
+				fields := strings.Split(key, delimiter)
+				if len(fields) == 0 {
+					continue
+				}
+			}
+			if len(fields) <= field-1 {
+				continue
+			}
+			key = fields[field-1]
+		}
+		// ignore empty string at the end of the input
+		if len(key) > 0 {
+			hist.Add(key)
+		}
+	}
+	return scanner.Err()
 }
