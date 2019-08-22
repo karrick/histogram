cb23fdba395ad64e3b89fb1932832875ef909794
Karrick S. McDermott <karrick@karrick.net>
2019-08-22 14:59:33 -0400

    handles newline better

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/karrick/histogram
 go 1.12
 
 require (
-	github.com/karrick/gobls v1.2.0
+	github.com/karrick/gobls v1.3.1
 	github.com/karrick/gohistogram v0.3.1
 	github.com/karrick/golf v1.2.0
-	github.com/karrick/gorill v1.9.1
+	github.com/karrick/gorill v1.10.1
 	github.com/karrick/gows v0.3.0
 )
--- main.go
+++ main.go
@@ -60,9 +60,37 @@ func main() {
 			briefUsage()
 			os.Exit(2)
 		}
-		stderr("%s.\n", err)
+		stderr("%s", newline(err.Error()))
 		os.Exit(1)
 	}
+}
+
+// newline returns a string with exactly one terminating newline character.
+// More simple than strings.TrimRight.  When input string has multiple newline
+// characters, it will strip off all but first one, reusing the same underlying
+// string bytes.  When string does not end in a newline character, it returns
+// the original string with a newline character appended.
+func newline(s string) string {
+	l := len(s)
+	if l == 0 {
+		return "\n"
+	}
+
+	// While this is O(length s), it stops as soon as it finds the first non
+	// newline character in the string starting from the right hand side of the
+	// input string.  Generally this only scans one or two characters and
+	// returns.
+
+	for i := l - 1; i >= 0; i-- {
+		if s[i] != '\n' {
+			if i+1 < l && s[i+1] == '\n' {
+				return s[:i+2]
+			}
+			return s[:i+1] + "\n"
+		}
+	}
+
+	return s[:1] // all newline characters, so just return the first one
 }
 
 // stderr formats and prints its arguments to standard error after prefixing
