5ad7f8f9dc79f464597fbe05edd32784b351f25a
Karrick S. McDermott <karrick@karrick.net>
2019-11-19 17:47:23 -0500

    remove commented out line

--- debug_development.go
+++ debug_development.go
@@ -9,6 +9,5 @@ import (
 
 // debug formats and prints arguments to stderr for development builds
 func debug(f string, a ...interface{}) {
-	// fmt.Fprintf(os.Stderr, f, a...)
 	os.Stderr.Write([]byte(ProgramName + ": " + fmt.Sprintf(f, a...)))
 }
